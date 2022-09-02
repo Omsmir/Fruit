b616c75b99eedbdc814d6e32a284106838b63220
OmarSamir <omarsamiir@outlook.com>
2022-09-02 14:30:06 +0200

    New Update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let li = document.querySelectorAll("ul li a")
+let li = document.querySelectorAll("nav ul li a")
 let image = document.querySelectorAll("#images .pic img")
 
 let headerImage = document.querySelector("header img")
@@ -14,6 +14,12 @@ let About = document.getElementById("About")
 
 let nav = document.querySelector(".navbar")
 
+
+let years = document.querySelectorAll(".years ul li")
+
+
+let circle = document.querySelector(".circle article h2")
+
 li.forEach((ele) => {
    ele.addEventListener("click" ,() => {
     if(ele.classList.contains("active")){
@@ -27,8 +33,6 @@ li.forEach((ele) => {
 })
 
 
-
-console.log(section)
 
 
 image.forEach((ele) => {
@@ -71,14 +75,13 @@ plus.addEventListener("click",() => {
 
   AOS.init();
 
-
 console.log(section[2].offsetTop)
 
  onscroll = function(){
 
     let position = document.documentElement.scrollTop
 
-        if(position >= section[1].offsetHeight && position < section[1].offsetHeight + section[2].offsetHeight) {
+        if(position >= section[1].offsetHeight ) {
                 nav.classList.add("back");
         }else {
             nav.classList.remove("back")
@@ -115,4 +118,117 @@ var removeAllActive = function(para){
   }
 
 
-console.log(nav)
+
+
+  let ctx = document.getElementById('myChart')
+  let myChart = new Chart(ctx, {
+      type: 'bar',
+     data: {
+        labels:["Jul","Aug",'Sep',"Oct","Nov","Dec"],
+        datasets: [{
+            label:"Last 6 Months",
+            data: [185, 1825, 500 ,1400 ,815 ,745],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+            barThickness: 50,
+            borderRadius: 5,
+            hoverBackgroundColor: '#0593F9',
+
+        }]
+     },
+      options: {
+        animations: {
+            borderColor: {
+              duration: 1000,
+              easing: 'easeInOut',
+              from: 1,
+              to: 5,
+              loop: true
+            }
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  
+  console.log(circle)
+
+    years.forEach((ele) => {
+        ele.addEventListener("click" , () =>{
+            if(ele.classList.contains("active")){
+                GenerateData(ele)
+                console.log(ele.attributes.id.value)
+                
+            }else {
+              removeAllActive(years)
+                ele.classList.add("active")
+                GenerateData(ele)
+                circle.innerText = `Our Sales ${ele.attributes.id.value}`
+
+            }
+        })
+    })
+
+
+
+function GenerateData(id){
+    let select = id.attributes["data-attr"].value
+    
+    
+    let array = select.split(" ").map((ele) => Number(ele))
+    let data = myChart.config.data;
+
+    data.datasets[0].data = array;  
+    
+    myChart.update()
+ }
+
+
+
+ let windw = window.matchMedia("(max-width:1198px")
+
+
+function match(x){
+    let nodata = myChart.config.data;
+
+    if(x.matches){
+
+        nodata.datasets[0].barThickness = 25
+
+    }else {
+        nodata.datasets[0].barThickness = 50
+    }
+    myChart.update()
+
+}
+
+
+windw.addListener(match)
+
+
+var typed = new Typed('span.auto', {
+    // Waits 1000ms after typing "First"
+    strings: ['First ^1000 sentence.', 'healthy',"Nice"],
+    typeSpeed:200,
+    backSpeed: 200,
+    loop: true,
+    smartBackspace: true 
+  });
